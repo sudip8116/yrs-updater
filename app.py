b07611e9c85968c4ae3d0a0c2c6ff12310082761
sudip8116 <127326011+sudip8116@users.noreply.github.com>
2025-10-13 22:40:10 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+from threading import Thread
+from time import sleep
+from flask import Flask
+import requests as rq
+
+
+main_site_url = "http://10.81.99.50:5000/request-update"
+headers = {"auth": "3f9a7b2c1d8e4f6a0b9c2d7e8f1a3b"}
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Welcome"
+
+
+def request_update():
+    while True:
+        try:
+            res = rq.get(main_site_url, headers=headers)
+            print(f"{res.status_code} : {res.text}")
+        except:
+            print("server down")
+        sleep(1)
+
+
+def request_update_thread():
+    Thread(target=request_update).start()
+
+
+request_update_thread()
